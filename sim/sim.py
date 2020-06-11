0a16ffc414bca2f54a84a3d43d49091e03f60b33
Safin Singh <42120904+safinsingh@users.noreply.github.com>
2020-06-11 13:02:54 -0700

    readability

--- sim/sim.py
+++ sim/sim.py
@@ -12,11 +12,14 @@ class Sim(SpecialThreeDScene):
         data = pd.read_csv("sim.csv")
         xVals = data.iloc[:, 0:1].values
         yVals = data.iloc[:, 1].values
+        
         poly = PolynomialFeatures(degree=8)
         x_poly = poly.fit_transform(xVals)
         poly.fit(x_poly, yVals)
+        
         lin = LinearRegression()
         lin.fit(x_poly, yVals)
+        
         customrate = bezier(lin.predict(poly.fit_transform(xVals)))
 
         obj = Sphere().move_to(np.array([ENDVALUEHERE, 0, 0]))
