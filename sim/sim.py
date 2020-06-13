100d23b0b86a0d29d759a1d4ee2c89eb8e864711
safinsingh <safin.singh@gmail.com>
2020-06-13 15:48:59 -0700

    use expanded dict for precision

--- sim/sim.py
+++ sim/sim.py
@@ -6,20 +6,19 @@ from sklearn.preprocessing import PolynomialFeatures
 
 class Sim(SpecialThreeDScene):
     def construct(self):
-        self.set_camera_orientation(
-            phi=1.2217304763960306, theta=-1.9198621771937625)
+        self.set_camera_orientation(**self.get_default_camera_position())
 
         data = pd.read_csv("sim.csv")
         xVals = data.iloc[:, 0:1].values
         yVals = data.iloc[:, 1].values
-        
+
         poly = PolynomialFeatures(degree=8)
         x_poly = poly.fit_transform(xVals)
         poly.fit(x_poly, yVals)
-        
+
         lin = LinearRegression()
         lin.fit(x_poly, yVals)
-        
+
         customrate = bezier(lin.predict(poly.fit_transform(xVals)))
 
         obj = Sphere().move_to(np.array([ENDVALUEHERE, 0, 0]))
